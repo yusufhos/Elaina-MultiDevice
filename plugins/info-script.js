946ae62eac8b49d2d06ceb5e22e569845e6887dc
yusufhos <107295216+yusufhos@users.noreply.github.com>
2022-09-30 21:40:33 +0700

    Update info-script.js

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -4,11 +4,11 @@ import fetch from 'node-fetch'
 let handler = async(m, { conn, text, usedPrefix, command }) => {
 let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
 
-let str = `*https://github.com/ImYanXiao/Elaina-MultiDevice*`
+let str = `*BUY BANG 30K*`
 let wibu = `https://hadi-api.herokuapp.com/api/loli` 
 let thumb = await(await fetch(wibu)).buffer()
 conn.sendButtonDoc(m.chat, str, wm,'ᴏᴋ','Bilek', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
-    mediaUrl: "https://Instagram.com/Xiao_yan_21",
+    mediaUrl: "https://Instagram.com/muhyusuff19",
     title: 'Elaina-MultiDevice',
     body: 'want source code?',
     thumbnail: thumb,
